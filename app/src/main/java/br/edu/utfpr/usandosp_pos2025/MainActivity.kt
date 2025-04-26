1ac9353be3f3374443939d480cd4019a3d888597
robisoncrisbrito <robison.brito@gmail.com>
2025-04-26 09:01:06 -0300

    completo

--- app/src/main/java/br/edu/utfpr/usandosp_pos2025/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/usandosp_pos2025/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.edu.utfpr.usandosp_pos2025
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -14,11 +16,26 @@ class MainActivity : AppCompatActivity() {
 
     private var ligado = false
 
+    private lateinit var sharedPreference : SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         imageView = findViewById( R.id.imageView )
+
+        sharedPreference = getSharedPreferences(
+            "properties",
+            Context.MODE_PRIVATE
+        )
+
+        ligado = sharedPreference.getBoolean( "ligado", false )
+
+        when ( ligado ) {
+            true  -> imageView.setImageResource( android.R.drawable.star_big_on )
+            false -> imageView.setImageResource( android.R.drawable.star_big_off )
+        }
+
     }
 
     fun btOnOffOnClick(view: View) {
@@ -33,6 +50,12 @@ class MainActivity : AppCompatActivity() {
                 ligado = true
             }
         }
+
+        val editor = sharedPreference.edit()
+        editor.putBoolean( "ligado", ligado )
+        editor.apply()
+
+
 
     }
 }
